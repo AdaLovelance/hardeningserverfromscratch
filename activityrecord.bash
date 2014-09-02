846f2feb6e05f486266cb4056a3438d661ffe2ca
root <root@ns398994.ip-37-59-43.eu>
2014-09-02 02:49:15 +0200

    activityrecord.bash

---
+++ activityrecord.bash
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#Este script está bajo licencia GPL y se distribuye sin garantías, copia, modifica y haz lo que quieras con él.
+#Este script configura los requisitos mínimos de seguridad de un servidor SSH, creado en Ubuntu 14.04
+#Creado por Kao y M 2/09/2014
+
+echo
+
+echo    '###############################################################################################'
+echo    '#                                 Server Users Activity Record 			       #'
+echo    '# Grabo lo que hacen los usuarios en la shell y lo guardo en un directorio oculto en /opt     #'
+echo    '#					      BY  K&M					       #'
+echo -e '############################################################################################### \n'
+
+aptitude install ttyrec
+
+#Configuración de TTYREC
+echo "TTYREC es  un shell script que permite grabación de sesiones de terminal"
+read -p "Introduzca un directorio donde desa que se guarden las grabaciones de las sessiónes ssh " dirgrab
+
+#Creamos un primer fichero de grabación y el diretorio donde se van a guardar las grabaciones
+mkdir -p /usr/share/audit/$dirgrab
+##ttyrec -u /root/audit/archivotest
+chmod -R o+w /usr/share/audit/*
+
+#Creamos el script que grabará las sesiones en /etc/profile.d
+cat > /etc/profile.d/ttyrec.sh  <<EOF
+
+if [ "/usr/share/audit/archivotest 'id -u' " != 0 ]
+then
+TTYFORMAT="/usr/share/audit/$dirgrab/\.${LOGNAME}\${HOSTNAME}\$(date +.%d·%h·%Y·%H:%M).rec"
+ttyrec -u \$TTYFORMAT
+fi
+
+EOF
+
+
